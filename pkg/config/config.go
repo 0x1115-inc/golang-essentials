14942642a558e48c77cb86a6a97a3a1723445a74
Adam <thebeast@vv0lll.org>
2024-12-05 16:36:31 +0700

    init

---
+++ pkg/config/config.go
@@ -0,0 +1,13 @@
+package config
+
+const DefaultConfigName = "ipg"
+
+type VConfig interface {
+	GetInt(string) int
+	GetInt64(string) int64
+	GetUint64(string) uint64
+	GetFloat64(string) float64
+	GetString(string) string
+	GetStringSlice(string) []string
+	GetStringMap(string) map[string]interface{}
+}
---
+++ pkg/config/env.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+type EnvConfig struct{}
+
+func (e *EnvConfig) GetInt(key string) int {
+	val, _ := strconv.Atoi(os.Getenv(key))
+	return val
+}
+
+func (e *EnvConfig) GetInt64(key string) int64 {
+	val, _ := strconv.ParseInt(os.Getenv(key), 10, 64)
+	return val
+}
+
+func (e *EnvConfig) GetUint64(key string) uint64 {
+	val, _ := strconv.ParseUint(os.Getenv(key), 10, 64)
+	return val
+}
+
+func (e *EnvConfig) GetFloat64(key string) float64 {
+	val, _ := strconv.ParseFloat(os.Getenv(key), 64)
+	return val
+}
+
+func (e *EnvConfig) GetString(key string) string {
+	return os.Getenv(key)
+}
+
+func (e *EnvConfig) GetStringSlice(key string) []string {
+	return strings.Split(os.Getenv(key), ",")
+}
+
+func (e *EnvConfig) GetStringMap(key string) map[string]interface{} {
+	result := make(map[string]interface{})
+	pairs := strings.Split(os.Getenv(key), ",")
+	for _, pair := range pairs {
+		kv := strings.SplitN(pair, "=", 2)
+		if len(kv) == 2 {
+			result[kv[0]] = kv[1]
+		}
+	}
+	return result
+}
