3d777173b8d752cafe9c61491d891dc05cea9712
Belle <beauty@vv0lll.org>
2024-12-24 19:41:11 +0700

    Refactor GetLogger function to improve parameter validation and type conversion for log level

--- pkg/logger/factory.go
+++ pkg/logger/factory.go
@@ -1,10 +1,26 @@
 package logger
 
-func GetLogger(args map[string]interface{}) VLogger {
-	switch args["provider"].(string) {
+import (
+	"fmt"
+	"strconv"
+)
+
+func GetLogger(provider string, args map[string]interface{}) VLogger {
+
+	// Parameter validation
+	logLevel, err := strconv.ParseInt(fmt.Sprintf("%s", args["level"]), 10, 64)
+	if err != nil {
+		return nil
+	}
+
+	if logLevel < LevelDebug || logLevel > LevelFatal {
+		return nil
+	}
+
+	switch provider {
 	case "simple":
-		return NewSimpleLogger(args["level"].(int))
+		return NewSimpleLogger(int(logLevel))
 	default:
 		return nil
-	}	
+	}
 }
